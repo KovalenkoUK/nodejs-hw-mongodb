5ac4db974533971ed5f4a8a1a31ec7e09b8a84a8
Matvey Kovalenko <kovalenko.matvey96@gmail.com>
2025-07-12 09:41:13 +0300

    1

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const SORT_ORDER = ['asc', 'desc'];
-export const CONTACT_TYPE = ['work', 'home', 'personal'];
+export const sortOrder = ['asc', 'desc'];
+export const contactType = ['work', 'home', 'personal'];
